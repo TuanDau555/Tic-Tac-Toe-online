0a7179baecb329df437e9cced588a3c2c097b792
TuanDau555 <nguyenmtuan505@gmail.com>
2025-06-15 18:24:01 +0700

    Add JoinRelay, and NGO

--- Assets/Scripts/GameManager/GameRelayManager.cs
+++ Assets/Scripts/GameManager/GameRelayManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
@@ -29,20 +32,36 @@ public class GameRelayManager : SingletonPersistent<GameRelayManager>
             joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
 
             // we use dtls protocol 
-            RelayServerEndpoint dtlsEndpoint = allocation.ServerEndpoints.First(
-                conn => conn.ConnectionType == "dtls"
-            );
+            RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
 
-            ip = dtlsEndpoint.Host;
-            port = dtlsEndpoint.Port;
-            connectionData = allocation.ConnectionData;
-            allocationId = allocation.AllocationId;
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
+
+            // After the host click Start game
+            // We create allocation and start host
+            NetworkManager.Singleton.StartHost();
+
+            return joinCode;
+        }
+        catch (RelayServiceException e)
+        {
+            Debug.Log(e);
+            return null;
+        }
+    }
+
+    public async void JoinRelay()
+    {
+        try
+        {
+            JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+
+            RelayServerData relayServerData = new RelayServerData(joinAllocation, "dtls");
+
+            NetworkManager.Singleton.StartClient();
         }
         catch (RelayServiceException e)
         {
             Debug.Log(e);
         }
-
-        return joinCode;
     }
 }
